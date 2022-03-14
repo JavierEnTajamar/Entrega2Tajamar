5a8ed3fa2611a0cf3be3d7c3fb2e8e253120a22d
JavierEnTajamar <javier.munozvaquerizo@tajamar365.com>
2022-03-14 20:26:06 +0100

    Agregar archivos de proyecto.

---
+++ LightHouseV1/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightHouseV1.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Lightkeeper")]
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
